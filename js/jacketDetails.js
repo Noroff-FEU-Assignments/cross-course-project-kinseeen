7128b61fc853ceaa7aeb0a2feb62bf2c2152075e
kinseeen <kine.jakobsen@hotmail..no>
2022-10-28 17:51:57 +0200

    fixed
    ,

--- js/jacketDetails.js
+++ js/jacketDetails.js
@@ -9,17 +9,12 @@ console.log(id);
 const titleContainer = document.querySelector(".nameContainer");
 const detailsContainer = document.querySelector(".jacketDetails");
 
-async function jacketInformation () {
+async function jacketInformation (test) {
     try {
-        const response = await fetch (`https://rainydays.kinejakobsen.tech/wp-json/wc/v3/products/${id}?consumer_key=ck_3c0d30cdff7e3750655598c1be8392865f8bf3f8&consumer_secret=cs_8aac1bee83badcf77a5c6b8cf5ef1b5914ab92ad`)
-        const reposonseAsJson = response.json();
+        const response = await fetch (`https://rainydays.kinejakobsen.tech/wp-json/wc/v3/products/${test}?consumer_key=ck_3c0d30cdff7e3750655598c1be8392865f8bf3f8&consumer_secret=cs_8aac1bee83badcf77a5c6b8cf5ef1b5914ab92ad`)
+        const reposonseAsJson = await response.json();
         console.log(reposonseAsJson);
-        detailsContainer.innerHTML = "";
-        setTitle(reposonseAsJson.name);
-        for (let i = 0; i < reposonseAsJson.length; i++) {
-            console.log(reposonseAsJson[i]);
-            createHTML(reposonseAsJson[i])
-        }
+        createHTML(reposonseAsJson)
         
     } catch (error) {
         detailsContainer.innerHTML = "an error has occured";
